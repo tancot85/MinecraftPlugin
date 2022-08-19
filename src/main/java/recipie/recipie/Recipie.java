59ff0657dd63a396fb179e93c8b155d7ae8b3937
Tanmay Tete <tanmay.tete@somaiya.edu>
2022-08-19 15:42:30 +0530

    no clue

--- src/main/java/recipie/recipie/Recipie.java
+++ src/main/java/recipie/recipie/Recipie.java
@@ -1,6 +1,9 @@
 package recipie.recipie;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import recipie.recipie.handler.OpEnchants;
 import recipie.recipie.handler.OpRootedDirt;
@@ -11,6 +14,14 @@ public final class Recipie extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("hello there");
+
+        Material[]  mlist = Material.values();
+        for(int i=0;i<10;i++){
+            ItemStack stack = new ItemStack(mlist[i],1);
+            ItemMeta meta = stack.getItemMeta();
+
+            Bukkit.getLogger().info(meta.getAsString());
+        }
         OpEnchants opEnchants = new OpEnchants();
         opEnchants.init();
         new OpRootedDirt(this);
